9f997d3d88408ff52f47323691e1494d737cc639
Jiří Suchomel <suchomel.jiri@gmail.com>
2020-02-13 16:22:05 +0100

    Basic authorization: only admin is allowed to add new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
 
   private
 
@@ -7,8 +8,13 @@ class ApplicationController < ActionController::Base
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
   end
 
+  def authorize_admin
+    if current_user.nil? || !current_user.admin?
+      redirect_to login_url, alert: "Only admin can add new users"
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 end
